671f388f85a39251f1c30c48386ce4a2473bc57c
Pedro Lucas <pl.maia@hotmail.com>
2020-07-27 18:51:40 -0300

    Added the project

---
+++ ASPMovie/ASPMovie/Data/ASPMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ASPMovie.Models;
+
+namespace ASPMovie.Data
+{
+    public class ASPMovieContext : DbContext
+    {
+        public ASPMovieContext (DbContextOptions<ASPMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ASPMovie.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ ASPMovie/ASPMovie/Models/SeedData.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ASPMovie.Data;
+using System;
+using System.Linq;
+
+
+namespace ASPMovie.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using(var context= new ASPMovieContext(serviceProvider.GetRequiredService<DbContextOptions<ASPMovieContext>>()))
+            {
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+                context.Movie.AddRange
+                    (
+                        new Movie
+                        {
+                            Title = "When Harry Met Sally",
+                            ReleaseDate = DateTime.Parse("1989-2-12"),
+                            Genre = "Romantic Comedy",
+                            Price = 7.99M,
+                            Rating="R"
+                        },
+                        new Movie
+                        {
+                            Title = "Ghostbusters",
+                            ReleaseDate = DateTime.Parse("1984-3-13"),
+                            Genre = "Comedy",
+                            Price =9.99M,
+                            Rating="R"
+                        },
+                        new Movie
+                        {
+                            Title = "Ghostbusters 2",
+                            ReleaseDate = DateTime.Parse("1986-2-23"),
+                            Genre = "Comedy",
+                            Price =9.99M,
+                            Rating = "R"
+                        },
+                        new Movie
+                        {
+                            Title = "Rio Bravo",
+                            ReleaseDate=DateTime.Parse("1959-4-15"),
+                            Genre="Western",
+                            Price=3.99M,
+                            Rating = "R"
+                        }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
